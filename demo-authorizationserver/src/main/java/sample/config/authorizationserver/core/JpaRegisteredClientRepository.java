cf010f4163ca1e7199ed1266f6931d9929dc713c
lenh0205 <lenhtranchi98@gmail.com>
2024-12-12 09:28:33 +0700

    authorization server - migrate embedded database H2 to SQL Server

--- demo-authorizationserver/src/main/java/sample/config/authorizationserver/core/JpaRegisteredClientRepository.java
+++ demo-authorizationserver/src/main/java/sample/config/authorizationserver/core/JpaRegisteredClientRepository.java
@@ -1,9 +1,6 @@
 package sample.config.authorizationserver.core;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.Module;
@@ -13,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.jackson2.SecurityJackson2Modules;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.jackson2.OAuth2AuthorizationServerJackson2Module;
@@ -38,6 +36,8 @@ public class JpaRegisteredClientRepository implements RegisteredClientRepository
         List<Module> securityModules = SecurityJackson2Modules.getModules(classLoader);
         this.objectMapper.registerModules(securityModules);
         this.objectMapper.registerModule(new OAuth2AuthorizationServerJackson2Module());
+
+        initializeDefaultClients();
     }
 
     @Override
@@ -159,5 +159,63 @@ public class JpaRegisteredClientRepository implements RegisteredClientRepository
             return ClientAuthenticationMethod.NONE;
         }
         return new ClientAuthenticationMethod(clientAuthenticationMethod);      // Custom client authentication method
+    }
+
+    private void initializeDefaultClients() {
+        List<RegisteredClient> defaultClients = List.of(
+                RegisteredClient.withId(UUID.randomUUID().toString())
+                        .clientId("messaging-client")
+                        .clientSecret("{noop}secret")
+                        .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                        .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                        .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                        .redirectUri("http://127.0.0.1:8080/login/oauth2/code/messaging-client-oidc")
+                        .redirectUri("http://127.0.0.1:8080/authorized")
+                        .postLogoutRedirectUri("http://127.0.0.1:8080/logged-out")
+                        .scope(OidcScopes.OPENID)
+                        .scope(OidcScopes.PROFILE)
+                        .scope("message.read")
+                        .scope("message.write")
+                        .scope("user.read")
+                        .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                        .build(),
+                RegisteredClient.withId(UUID.randomUUID().toString())
+                        .clientId("device-messaging-client")
+                        .clientAuthenticationMethod(ClientAuthenticationMethod.NONE)
+                        .authorizationGrantType(AuthorizationGrantType.DEVICE_CODE)
+                        .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                        .scope("message.read")
+                        .scope("message.write")
+                        .build(),
+                RegisteredClient.withId(UUID.randomUUID().toString())
+                        .clientId("token-client")
+                        .clientSecret("{noop}token")
+                        .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                        .authorizationGrantType(new AuthorizationGrantType("urn:ietf:params:oauth:grant-type:token-exchange"))
+                        .scope("message.read")
+                        .scope("message.write")
+                        .build(),
+                RegisteredClient.withId(UUID.randomUUID().toString())
+                        .clientId("mtls-demo-client")
+                        .clientAuthenticationMethod(ClientAuthenticationMethod.TLS_CLIENT_AUTH)
+                        .clientAuthenticationMethod(ClientAuthenticationMethod.SELF_SIGNED_TLS_CLIENT_AUTH)
+                        .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                        .scope("message.read")
+                        .scope("message.write")
+                        .clientSettings(ClientSettings.builder()
+                                .x509CertificateSubjectDN("CN=demo-client-sample,OU=Spring Samples,O=Spring,C=US")
+                                .jwkSetUrl("http://127.0.0.1:8080/jwks")
+                                .build())
+                        .tokenSettings(TokenSettings.builder()
+                                .x509CertificateBoundAccessTokens(true)
+                                .build())
+                        .build()
+        );
+        for (RegisteredClient client : defaultClients) {
+            if (findByClientId(client.getClientId()) == null) {
+                save(client);
+            }
+        }
     }
 }
